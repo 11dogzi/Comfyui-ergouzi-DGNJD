f5c5d5ecba15b3dd3ff55aba2c80bafc6c7cf1c5
11dogzi <695660735@qq.com>
2024-03-02 00:27:02 +0800

    上传说明图像

--- nodes/egcchq.py
+++ nodes/egcchq.py
@@ -8,22 +8,22 @@ class EGTXCCHQ:
     @classmethod
     def INPUT_TYPES(cls):
         return {
-            
+            # 必选输入
             "required": {
-                "输入图像": ("IMAGE", {}), 
+                "image_in": ("IMAGE", {}), 
             }
         }
 
     RETURN_TYPES = ("INT", "INT")
     RETURN_NAMES = ("宽度", "高度") 
     FUNCTION = "get_image_size"
-    CATEGORY = "2🐕/图像/常规处理" 
+    CATEGORY = "2🐕/图像" 
 
-    def get_image_size(self, 输入图像: torch.Tensor) -> Tuple[int, int]:
+    def get_image_size(self, image_in: torch.Tensor) -> Tuple[int, int]:
         if len(image_in.shape) == 4:
-            height, width = 输入图像.shape[1], 输入图像.shape[2]
+            height, width = image_in.shape[1], image_in.shape[2]
         else:
-            height, width = 输入图像.shape[-2], 输入图像.shape[-1]
+            height, width = image_in.shape[-2], image_in.shape[-1]
         return (width, height)
 
 NODE_CLASS_MAPPINGS = { "EG_TX_CCHQ" : EGTXCCHQ }
