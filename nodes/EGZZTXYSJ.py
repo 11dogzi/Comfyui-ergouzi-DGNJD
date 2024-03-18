76568b20a56a3e66a1335bc1cc9886ddcda9a3e2
xiake1111 <695660735@qq.com>
2024-03-18 14:59:07 +0800

    添加透明图像加载

--- nodes/EGZZTXYSJ.py
+++ nodes/EGZZTXYSJ.py
@@ -1,18 +1,12 @@
 import importlib.util
+import os
 from pathlib import Path
-# 获取当前文件的绝对路径
 current_file_path = Path(__file__).resolve()
-# 获取当前文件的目录
 current_directory = current_file_path.parent
-# 获取folder_paths.py文件的目录，现在是曾祖父目录
 folder_paths_directory = current_directory.parent.parent.parent / "folder_paths.py"
-# 创建一个模块规范（spec），指定folder_paths.py文件的路径
 spec = importlib.util.spec_from_file_location("folder_paths", str(folder_paths_directory))
-# 从模块规范中创建一个模块对象
 folder_paths = importlib.util.module_from_spec(spec)
-# 执行模块中的代码
 spec.loader.exec_module(folder_paths)
-# 从folder_paths模块中导入所有内容
 from folder_paths import *
 from PIL import Image, ImageOps, ImageSequence, PngImagePlugin
 from PIL.ExifTags import TAGS
@@ -27,7 +21,8 @@ class EGTXFT:
         input_dir = folder_paths.get_input_directory()
         files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
         return {"required":
-                    {"image": (sorted(files), {"image_upload": True})},
+                    {"image": (sorted(files), {"image_upload": True}),
+                     "load_alpha": (["RGB", "RGBA"], {"default": "RGB", "widget": "select"}),}
                 }
 
     CATEGORY = "2🐕/图像"
@@ -35,28 +30,22 @@ class EGTXFT:
     RETURN_TYPES = ("IMAGE", "MASK", "STRING")
     FUNCTION = "load_image"
     def get_metadata_text(self, img):
-        # Get the image's PngInfo data
         pnginfo_data = img.info
-        # If the image has no PngInfo data, return a specific message
         if not pnginfo_data:
             return "No metadata found in the image."
-        # Parse the PngInfo data to a readable text format
         metadata_text = ""
         for key, value in pnginfo_data.items():
-            # Check if the value is a JSON string and attempt to decode it
             try:
-                # Ensure the value is a string before attempting to decode JSON
                 if not isinstance(value, (str, bytes, bytearray)):
                     value = str(value)
                 value = json.loads(value)
                 if isinstance(value, dict):
-                    value = json.dumps(value, indent=4)  # Pretty print the JSON dict
+                    value = json.dumps(value, indent=4)
             except (json.JSONDecodeError, TypeError):
-                pass  # If it's not a JSON string, just use the original value
+                pass
             metadata_text += f"{key}: {value}\n"
-        # Return the metadata text after the loop
         return metadata_text.strip()
-    def load_image(self, image):
+    def load_image(self, image, load_alpha):
         image_path = folder_paths.get_annotated_filepath(image)
         img = Image.open(image_path)
         metadata_text = self.get_metadata_text(img)
@@ -66,14 +55,19 @@ class EGTXFT:
             i = ImageOps.exif_transpose(i)
             if i.mode == 'I':
                 i = i.point(lambda i: i * (1 / 255))
-            image = i.convert("RGB")
-            image = np.array(image).astype(np.float32) / 255.0
-            image = torch.from_numpy(image)[None,]
+            if load_alpha == "RGBA" and 'A' in i.getbands():
+                image = i.convert("RGBA")
+                image = np.array(image).astype(np.float32) / 255.0
+                image = torch.from_numpy(image)[None,]
+            else:
+                image = i.convert("RGB")
+                image = np.array(image).astype(np.float32) / 255.0
+                image = torch.from_numpy(image)[None,]
             if 'A' in i.getbands():
                 mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
                 mask = 1. - torch.from_numpy(mask)
             else:
-                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+                mask = torch.ones((64,64), dtype=torch.float32, device="cpu")
             output_images.append(image)
             output_masks.append(mask.unsqueeze(0))
         if len(output_images) > 1:
